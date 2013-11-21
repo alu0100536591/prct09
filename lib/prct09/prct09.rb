2d93be5b0f75f58cacc09bbbec992bee2564af4c
Gonzalo Garcia <alu0100403619@ull.edu.es>
2013-11-21 12:11:45 +0000

    Cambios en /lib/prct09/prct09.rb

--- lib/prct09/prct09.rb
+++ lib/prct09/prct09.rb
@@ -3,45 +3,52 @@ require "lib/prct09/Matriz.rb"
 
 	MatrizMadre = Matriz.new()
 	puts "MATRICES DENSAS"
+	puts ""
 	puts "Mat_A"
 	Mat_A = MatrizMadre.convert([[1, 2], [3, 4]])
-	puts Mat_A.to_s
-	puts Mat_A.class
+	puts "#{Mat_A}"
+	puts "Clase de Mat_A: #{Mat_A.class}"
+	
+	puts ""
 	
 	puts "Mat_B"
 	Mat_B = MatrizMadre.convert([[2, 3], [7, 1]])
-	puts Mat_B.to_s
-	puts Mat_B.class
+	puts "#{Mat_B}"
+	puts "Clase de Mat_B: #{Mat_B.class}"
 	
 	puts ""
+	
 	puts "Sum A+B:"
-	puts (Mat_A + Mat_B).to_s
+	puts "#{(Mat_A + Mat_B)}"
 	
 	puts ""
+	
 	puts "Prod A*B:"
-	puts (Mat_A * Mat_B).to_s
+	puts "#{(Mat_A * Mat_B)}"
 	
 	#---------------------------------------------
 	
 	puts "MATRICES DISPERSAS"
+	puts ""
 	puts "Mat_C"
 	Mat_C = MatrizMadre.convert([[1, 0], [0, 0]])
-	puts Mat_C.to_s
-	puts Mat_C.class
+	puts "#{Mat_C.to_s}"
+	puts "Clase de Mat_C: #{Mat_C.class}"
 
+	puts ""
 	
 	puts "Mat_D"
 	Mat_D = MatrizMadre.convert([[4, 0], [0, 0]])
-	puts Mat_D.to_s
-	puts Mat_D.class
+	puts "#{Mat_D}"
+	puts "Clase de Mat_D: #{Mat_D.class}"
 	
 	puts ""
 	puts "Sum C+D:"
-	puts (Mat_C + Mat_D).to_s
+	puts "#{(Mat_C + Mat_D)}"
 	
 	puts ""
 	puts "Prod C*D:"
-	puts (Mat_C * Mat_D).to_s
+	puts "#{(Mat_C * Mat_D)}"
 	
 	
 
