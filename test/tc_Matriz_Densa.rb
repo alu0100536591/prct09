313d3c4d8625f9ecd7c4ff4ae4215b09079b04fe
Gonzalo Garcia <alu0100403619@ull.edu.es>
2013-11-21 12:45:19 +0000

    Modificacion en el test para la clase Matriz_Densa

---
+++ test/tc_Matriz_Densa.rb
@@ -0,0 +1,34 @@
+# Implementar en este fichero las Pruebas Unitarias asociadas a la clase Matriz_Densa
+
+require "../lib/prct09/Matriz.rb"
+require "test/unit"
+
+class Test_Matriz_Densa < Test::Unit::TestCase
+  
+  def setup
+    MatrizMadre = Matriz.new()
+    @mat_A = MatrizMadre.convert([[1, 2], [3, 4]])
+    @mat_B = MatrizMadre.convert([[2, 3], [7, 1]])
+  end
+  
+  def test_initialize
+    assert_equal("1 2 \n3 4", @mat_A.to_s)
+    assert_equal("2 3 \n7 1", @mat_B.to_s)
+  end
+  
+  def test_clase
+    assert_equal("Matriz_Densa", @mat_A.class.to_s)
+    assert_equal("Matriz_Densa", @mat_B.class.to_s)
+  end
+  
+  def test_sum
+    m_sum = @mat_A + @mat_B
+    assert_equal("3 5 \n10 5", m_sum.to_s)
+  end
+  
+  def test_mult
+    m_mult = @mat_A * @mat_B
+    assert_equal("16 5 \n34 13", m_mult.to_s)
+  end
+  
+end
